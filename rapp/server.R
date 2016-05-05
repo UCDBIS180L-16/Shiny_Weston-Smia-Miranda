1a5439ae2a6f9f48ea61435dd0f76e41a23092f4
bis180l_local <wlselna@ucdavis.edu>
2016-05-05 16:25:24 -0700

    server v2

--- rapp/server.R
+++ rapp/server.R
@@ -8,19 +8,18 @@
 #
 
 library(shiny)
+load("SNP_lab_data.Rdata")
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
+  output$scatter <- renderPlot({
+      sub <- data.phenomds[,c(input$color,input$ax1,input$ax2)]
+      plt <- ggplot(sub, aes(input$ax1,input$ax2,color = input$color))
+      plt <- plt + geom_point()+ggtitle(paste("Scatterplot of", input$ax1,"and",input$ax2,"by", input$color, sep = " "))+scale_color_hue(c = input$slider)
   })
-  
+  output$box <- renderPlot({
+      sub <- data.phenomds[,c(input$ax1,input$ax2)]   # ax1 is discrete
+      plt <- ggplot(aes(x=input$ax1, y=input$ax2, color=input$ax1), data=sub)
+      plt <- plt + geom_boxplot()+ggtitle(paste("Box plot of", input$ax2,"by", input$ax1, sep = " "))+scale_color_hue(c = input$slider)
+  })
 })
