08714294a9db37baa65d4ec60cb2ef464e4eff68
shabshab <ssahebi@ucdavis.edu>
2016-05-05 17:03:07 -0700

    last changes 5/5

--- rapp/server.R
+++ rapp/server.R
@@ -8,7 +8,8 @@
 #
 
 library(shiny)
-load("SNP_lab_data.Rdata")
+library(ggplot2)
+data.phenomds <- load("SNP_lab_data.Rdata")
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
@@ -17,9 +18,12 @@ shinyServer(function(input, output) {
       plt <- ggplot(sub, aes(input$ax1,input$ax2,color = input$color))
       plt <- plt + geom_point()+ggtitle(paste("Scatterplot of", input$ax1,"and",input$ax2,"by", input$color, sep = " "))+scale_color_hue(c = input$slider)
   })
-  output$box <- renderPlot({
-      sub <- data.phenomds[,c(input$ax1,input$ax2)]   # ax1 is discrete
-      plt <- ggplot(aes(x=input$ax1, y=input$ax2, color=input$ax1), data=sub)
-      plt <- plt + geom_boxplot()+ggtitle(paste("Box plot of", input$ax2,"by", input$ax1, sep = " "))+scale_color_hue(c = input$slider)
+  output$summary <- renderPrint({
+    summary(data.phenomds)
   })
+#  output$box <- renderPlot({
+#      sub <- data.phenomds[,c(input$ax1,input$ax2)]   # ax1 is discrete
+#      plt <- ggplot(aes(x=input$ax1, y=input$ax2, color=input$ax1), data=sub)
+#      plt <- plt + geom_boxplot()+ggtitle(paste("Box plot of", input$ax2,"by", input$ax1, sep = " "))+scale_color_hue(c = input$slider)
+#  })
 })
--- rapp/ui.R
+++ rapp/ui.R
@@ -3,53 +3,52 @@
 # run the application by clicking 'Run App' above.
 #
 
-
-
-
 library(markdown)
+library(shiny)
 
 shinyUI(navbarPage("RAPP",
                    tabPanel("Scatter Plot",
                             sidebarLayout(
                               sidebarPanel(
-                                radioButtons("ax1", "Select your first trait", c("Alu.Tol", "Plant.height", "Seed.number.per.panicle"),
-                                radioButtons("ax2", "Select your Second trait", c("Alu.Tol", "Plant.height", "Seed.number.per.panicle"),
-                                radioButtons("color", "Select your feature", c("popID", "Region"),
+                                radioButtons("ax1", "Select your first trait", c("Alu.Tol", "Plant.height", "Seed.number.per.panicle")),
+                                radioButtons("ax2", "Select your Second trait", c("Alu.Tol", "Plant.height", "Seed.number.per.panicle")),
+                                radioButtons("color", "Select your feature to color by", c("popID", "Region")),
                                 sliderInput("slider", "Select your color scheme", min=1, max=360, value=360)
-                                )
-                              ),
-                              mainPanel(
-                                plotOutput("scatter")
+                                ),
+                              mainPanel(plotOutput("scatter")
                               )
+                              
                             )
-                   ),
+                   
+                        ),
                    tabPanel("Summary",
                             verbatimTextOutput("summary")
-                   ),
-                   navbarMenu("More",
-                              tabPanel("Table",
-                                       DT::dataTableOutput("table")
-                              ),
-                              tabPanel("About",
-                                       fluidRow(
-                                         column(6,
-                                                includeMarkdown("about.md")
-                                         ),
-                                         column(3,
-                                                img(class="img-polaroid",
-                                                    src=paste0("http://upload.wikimedia.org/",
-                                                               "wikipedia/commons/9/92/",
-                                                               "1919_Ford_Model_T_Highboy_Coupe.jpg")),
-                                                tags$small(
-                                                  "Source: Photographed at the Bay State Antique ",
-                                                  "Automobile Club's July 10, 2005 show at the ",
-                                                  "Endicott Estate in Dedham, MA by ",
-                                                  a(href="http://commons.wikimedia.org/wiki/User:Sfoskett",
-                                                    "User:Sfoskett")
-                                                )
-                                         )
-                                       )
-                              )
-                   )
-))
+                   )))
+                   #navbarMenu("More",
+                              #tabPanel("Table",
+                                       #DT::dataTableOutput("table")
+                             # ),
+                              #tabPanel("About",
+                               #        fluidRow(
+                                #         column(6,
+                                 #               includeMarkdown("about.md")
+                                  #       ),
+                                   #      column(3,
+                                    #            img(class="img-polaroid",
+                                     #               src=paste0("http://upload.wikimedia.org/",
+                                      #                         "wikipedia/commons/9/92/",
+                                       #                        "1919_Ford_Model_T_Highboy_Coupe.jpg")),
+                                        #        tags$small(
+                                         #         "Source: Photographed at the Bay State Antique ",
+                                          #        "Automobile Club's July 10, 2005 show at the ",
+                                          #        "Endicott Estate in Dedham, MA by ",
+                                           #       a(href="http://commons.wikimedia.org/wiki/User:Sfoskett",
+                                            #        "User:Sfoskett")
+                                             #   )
+                               #          )
+                              #         )
+                             # )
+ #                  )
+#))
 
+#))
